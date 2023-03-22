b2c31bac582b166d00f81e747a155e56892aca6f
Hujary <petzold.tom2000@gmail.com>
2023-03-22 17:15:43 +0100

    a

---
+++ src/components/Scanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p> test </p>
+  </div>
+</template>
+
+<script>
+ export default{
+      data() {
+        return {
+          data: 0
+        }
+      },
+      methods() {
+      },
+    }
+</script>
+
+<style>
+</style>
