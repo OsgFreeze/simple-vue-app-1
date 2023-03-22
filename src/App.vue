0b5422e39ab1b4d22d62b9872f3bb4ac2791a07c
Hujary <petzold.tom2000@gmail.com>
2023-03-22 18:03:44 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
     <div> 
-      <p> {{ ScannData }}</p>
+      <h1> Barcode App</h1>
+      <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+      <p> deine gescannte Information: {{ ScannData }}</p>
     </div>
 </template>
 
 <script>
 import { StreamBarcodeReader } from "vue-barcode-reader";
-
 export default {
   data() {
     return {
@@ -23,9 +23,8 @@ export default {
    methods: {
      onDecode (result) { 
       this.ScannData = result;
-      console.log(result) 
       if(result == "4059549000152"){
-        console.log("funktioniert");
+        console.log(this.Kreide);
       }  
     }
   }
